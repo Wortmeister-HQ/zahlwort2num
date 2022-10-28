a5475c9f176a66660b577aacdd7b6d4d1454f2e5
walu2 <piotr@deluxe-soft.com>
2022-10-28 13:58:36 +0200

    Added Linter & TestCase unit

--- zahlwort2num/tests/test_console.py
+++ zahlwort2num/tests/test_console.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
-from zahlwort2num.command_line import main
+from zahlwort2num.command_line import *
 
 class TestConsole(TestCase):
     def test_basic(self):
-        main()
+        self.assertRaises(ArgumentError, main)
